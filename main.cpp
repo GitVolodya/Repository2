ffb13d152651b6a5bef69a8f1632b8c05f5f4108
VOLODYA <vova.x23@mail.ru>
2023-03-15 12:22:13 +0300

    com2

--- main.cpp
+++ main.cpp
@@ -1,3 +1,13 @@
 #include<stdio.h>
 #include<conio.h>
 #include<locale.h>
+int main()
+{
+	setlocale(0, "rus");
+	int n;
+	char str1[3][10];
+	FILE* fp;
+	fp = fopen("D:\\ex.txt", "w+a");
+	puts("¬ведите число");
+	scanf("%d", &n);
+	fprintf(fp, "%d\n", n);
